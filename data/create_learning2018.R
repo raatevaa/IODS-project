5384938ac8b7f941e4e28296953345e35170ab85
Antti Raatevaara <42667763+raatevaa@users.noreply.github.com>
2018-11-09 11:17:16 +0200

    Chapter 2, modelling and plotting
    tasks for week 2

---
+++ data/create_learning2018.R
@@ -0,0 +1,27 @@
+#Antti Raatevaara
+#04.11.2018
+#This r-script holds the code for data wrangling exercise.
+
+# Read a delimited file
+df <- read.delim("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t") 
+
+#Short description of data frame:
+#Data is a table, which has 183 rows and 60 columns. Each column seems to represent a variable that has integer values, except
+#column gender is string (F/M). Age, attitude, points are continous variables, and other columns are probably categorical (1-5).
+
+#--------------
+
+#Select variables gender, age, attitude, deep, stra, surf and point
+
+myvars <- c("gender", "Age", "Attitude","Points")
+  #myvars2 <- c("gender", "Age", "Attitude","Deep","Stra","Surf","Points")
+#Subset
+newdata <- df[myvars]
+#Clear zeros
+newdata <- subset(newdata, Points > 0) 
+
+#Set working directory and write csv without indexing row
+setwd("/Users/Omistaja/Documents/GitHub/IODS-project/data")
+write.csv(newdata, file = "learning2014.csv",row.names=FALSE)
+#Read data with headers
+data_read <- read.csv(file="learning2014.csv", header=TRUE, sep=",")
