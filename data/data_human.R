8927c1a3920cbf56523dc3c19024a130b47bcf05
Antti Raatevaara <42667763+raatevaa@users.noreply.github.com>
2018-11-26 19:00:41 +0200

    Update data_human.R

--- data/data_human.R
+++ data/data_human.R
@@ -11,21 +11,8 @@ summary(data1)
 str(data2)
 summary(data2)
 
-colnames(data1)[3] <- "hdi"
-colnames(data1)[4] <- "Life.expectancy"
-colnames(data1)[5] <- "edu_expectancy"
-colnames(data1)[6] <- "edu_inYears"
-colnames(data1)[7] <- "gni"
-colnames(data1)[8] <- "gni_minus_hdi"
-
-colnames(data2)[3] <- "gii" 
-colnames(data2)[4] <- "mmr" 
-colnames(data2)[5] <- "abr" 
-colnames(data2)[6] <- "F.inParl" 
-colnames(data2)[7] <- "edu_2F" 
-colnames(data2)[8] <- "edu_2M" 
-colnames(data2)[9] <- "work_F"  
-colnames(data2)[10] <- "work_M"
+colnames(data1)<-c("rank","country","gii","mmr","birth","parliament", "edu_2F","edu_2M","Lab_F","Lab_M")
+colnames(data2)<-c("rank","country","HDI","LE","EYoE","MYoE","gni","GNIm_HDI")
 
 data2 <- mutate(data2, eduRatio = edu_2F / edu_2M) 
 data2 <- mutate(data2, workRatio = work_F / work_M)
