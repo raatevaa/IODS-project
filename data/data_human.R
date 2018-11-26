da95fe1c9c175a125c361b5eb1b33113b826abd5
Antti Raatevaara <42667763+raatevaa@users.noreply.github.com>
2018-11-26 18:54:34 +0200

    human data

--- data/data_human.R
+++ data/data_human.R
@@ -14,24 +14,24 @@ summary(data1)
 summary(data2)
 
 # Rename the variables with (shorter) descriptive names
-colnames(data1)[3] <- "HDI"    # Human Development Index
+colnames(data1)[3] <- "hdi"
 colnames(data1)[4] <- "Life.expectancy"
-colnames(data1)[5] <- "edu.expectancy"
-colnames(data1)[6] <- "edu.inYears"
-colnames(data1)[7] <- "GNI"    # Gross National Income
-colnames(data1)[8] <- "GNI_minus_HDI"
+colnames(data1)[5] <- "edu_expectancy"
+colnames(data1)[6] <- "edu_inYears"
+colnames(data1)[7] <- "gni"
+colnames(data1)[8] <- "gni_minus_hdi"
 
-colnames(data2)[3] <- "GII" 
-colnames(data2)[4] <- "MMR" 
-colnames(data2)[5] <- "ABR" 
+colnames(data2)[3] <- "gii" 
+colnames(data2)[4] <- "mmr" 
+colnames(data2)[5] <- "abr" 
 colnames(data2)[6] <- "F.inParl" 
-colnames(data2)[7] <- "edu2F" 
-colnames(data2)[8] <- "edu2M" 
-colnames(data2)[9] <- "workF"  
-colnames(data2)[10] <- "workM"
+colnames(data2)[7] <- "edu_2F" 
+colnames(data2)[8] <- "edu_2M" 
+colnames(data2)[9] <- "work_F"  
+colnames(data2)[10] <- "work_M"
 
-data2 <- mutate(data2, eduRatio = edu2F / edu2M) 
-data2 <- mutate(data2, workRatio = workF / workM)
+data2 <- mutate(data2, eduRatio = edu_2F / edu_2M) 
+data2 <- mutate(data2, workRatio = work_F / work_M)
 library(dplyr)
-human <- inner_join(gii, hd, by = "Country") # 195 observations and 19 variables. 
-write.csv(human, file="human.csv", row.names = FALSE) # write data to file
+human <- inner_join(data1, data2, by = "Country") 
+write.csv(human, file="human.csv", row.names = FALSE) 
